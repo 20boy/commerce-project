c0559ba6257935a18b5e5507932104deafcc3129
20boy <koyejooyerinde@gmail.com>
2023-12-04 10:20:16 -0800

    updated for deploying

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import "./pages/Home";
 import Home from "./pages/Home";
-import { Produce } from "./pages/Produce";
-import { ProductsLists } from "./pages/ProductsLists";
-import { Cart } from "./pages/cart";
+
 function App() {
   return (
     <div>
-      <Produce />
+      <Home />
     </div>
   );
 }
